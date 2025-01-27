99085dec9abd47b95e452b12e21385750f6a0567
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2025-01-27 18:04:11 +0700

    Build header p1

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+
+import styles from './AccountItem.module.scss';
+
+function AccountItem() {
+    return (
+        <div className={clsx(styles.wrapper)}>
+            <img
+                src='https://p16-sign-sg.tiktokcdn.com/aweme/1080x1080/tos-alisg-avt-0068/e7179a80746ed2dfcf9bdd5dbe0dc1a0.jpeg?lk3s=a5d48078&nonce=68981&refresh_token=cf9d3f74caf53aac71ba1db407cea34a&x-expires=1738141200&x-signature=sNphKTR2%2FIXq0oVl3mU7PgDRgmU%3D&shp=a5d48078&shcp=81f88b70'
+                alt=''
+                className={clsx(styles.avt)}
+            />
+            <div className={clsx(styles.infor)}>
+                <h4 className={clsx(styles.name)}>Nguyễn Văn A</h4>
+                <span className={clsx(styles.username)}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,12 +1,67 @@
-// import classNames from 'classnames';
+import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faCircleXmark,
+    faSpinner,
+    faMagnifyingGlass,
+} from '@fortawesome/free-solid-svg-icons';
+// import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'; // optional
+import Tippy from '@tippyjs/react/headless';
 import clsx from 'clsx';
 
 import styles from './Header.module.scss';
+import images from '@/assets/images';
+import { Wrapper as PopperWrapper } from '@/components/Popper';
+import AccountItem from '@/components/AccountItem';
 
 function Header() {
+    const [searchResults, setSearchResults] = useState([]);
+
+    useEffect(() => {
+        setSearchResults([1, 2]);
+    }, []);
+
     return (
         <header className={clsx(styles.wrapper)}>
-            <div className={clsx(styles.inner)}></div>
+            <div className={clsx(styles.inner)}>
+                <div className={clsx(styles.logo)}>
+                    <img src={images.logo} alt='tiktok' />
+                </div>
+                <Tippy
+                    interactive
+                    visible={searchResults.length > 0}
+                    render={(attrs) => (
+                        <div
+                            className={clsx(styles.searchResult)}
+                            tabIndex='-1'
+                            {...attrs}>
+                            <PopperWrapper>
+                                <h4 className={clsx(styles.searchTitle)}>
+                                    account
+                                </h4>
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                            </PopperWrapper>
+                        </div>
+                    )}>
+                    <div className={clsx(styles.search)}>
+                        <input placeholder='search account and video' />
+                        <button className={clsx(styles.clear)}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon
+                            className={clsx(styles.loading)}
+                            icon={faSpinner}
+                        />
+                        <button className={clsx(styles.searchButton)}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
+                <div className={clsx(styles.actions)}></div>
+            </div>
         </header>
     );
 }
